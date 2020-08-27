1087ea0f5b4ebbad81f91e70be09967d514772ed
TomF46 <t-d-f@live.co.uk>
2020-08-27 21:28:52 +0100

    Basic view goal scorers in frontend

--- FrontEnd/src/components/leagues/seasons/fixtures/FixtureDetail.js
+++ FrontEnd/src/components/leagues/seasons/fixtures/FixtureDetail.js
@@ -7,9 +7,23 @@ const FixtureDetail = ({ fixture }) => (
       {fixture.homeTeamName} - {fixture.awayTeamName}
     </h2>
     {fixture.complete ? (
-      <p>
-        Result: {fixture.homeScore} - {fixture.awayScore}
-      </p>
+      <>
+        <p>
+          Result: {fixture.homeScore} - {fixture.awayScore}
+        </p>
+        <p>{`${fixture.homeTeamName} scorers`}</p>
+        <ul>
+          {fixture.homeGoalScorers.map((goal, index) => {
+            return <li key={index}>{goal.playerDisplayName}</li>;
+          })}
+        </ul>
+        <p>{`${fixture.awayTeamName} scorers`}</p>
+        <ul>
+          {fixture.awayGoalScorers.map((goal, index) => {
+            return <li key={index}>{goal.playerDisplayName}</li>;
+          })}
+        </ul>
+      </>
     ) : (
       <p>Fixture not yet complete</p>
     )}
